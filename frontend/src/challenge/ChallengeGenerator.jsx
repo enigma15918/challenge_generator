18bc1e3e1da49fa363765fbfc9c2c9cc2033e1c0
enigma15918 <enigma15918@gmail.com>
2026-07-22 13:10:54 +0300

    end from intializing challenge

--- frontend/src/challenge/ChallengeGenerator.jsx
+++ frontend/src/challenge/ChallengeGenerator.jsx
@@ -1,7 +1,85 @@
 import "react"
+import {useState,useEffect} from "react"
+import MCQChallenge from "./MCQChallenge.jsx"
+
 
 export default function ChallengeGenerator(){
 
+    const [challenge,setChallenge]=useState(null)
 
+    const [isLoading,setIsLoading]=useState(false)
+
+    const [error,setError]=useState(null)
+
+    const [difficulty,setDifficulty]=useState("easy")
+
+    const [qouta,setQouta]=useState(null)
+
+    const fetchQouta =async() => {}
+
+    const generateChallenge = async () => {}
+
+    const getNextResetTime= () => {}
+
+    return <div className="challenge-container">
+
+            <h2>Coding Challenge Generator</h2>
+
+            <div className="qouta-display">
+
+                <p>Challenge remaining tody : {qouta?.qouta_remaining || 0}</p>
+
+                {qouta?.qouta_remaining === 0 && (
+                    <p>Next reset : {0}</p>
+                )}
+
+            </div>
+
+            <div className="difficulty-selector">
+                <label htmlFor="difficulty">
+                    Select Difficulty
+                </label>
+
+                <select  
+                         id="difficulty"
+                         value={difficulty}
+                         onChange={(e)=>setDifficulty(e.target.value)}
+                         disabled={isLoading}
+                
+                
+                >
+
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+
+                </select>
+
+            </div>
+            
+            <button
+
+                onClick={generateChallenge}
+
+                disabled={isLoading || qouta?.qouta_remaining ===0}
+
+                className="generate-button"
+            
+            >
+
+                {isLoading? "Generate....." : "Generate Challenge"}
+                
+            </button>
+
+
+            {error && <div className="error-message">
+                
+                <p>{error}</p>
+                
+                </div>}
+
+                {challenge && <MCQChallenge challenge={challenge}/>}
+
+    </div>
 
 }
--- frontend/src/challenge/MCQChallenge.jsx
+++ frontend/src/challenge/MCQChallenge.jsx
@@ -1,5 +1,75 @@
 import "react"
 
-export default function MCQChallenge(){
+import {useState} from "react"
+
+export default function MCQChallenge({challenge,showExplaination=false}){
+
+    const [selectedOption,setSelectedOption] = useState(null)
+
+    const [shouldShowExplaination,setShouldShowExplaination] = useState(showExplaination)
+
+
+    const  options= typeof challenge.options === "string" ? JSON.parse(challenge.options):challenge.options
+
+    const handleOptionSelect = (index) =>{
+        if (selectedOption !== null) return;
+        setSelectedOption(index)
+        setShouldShowExplaination(true)
+    }
+
+    const getOptionClass=(index)=>{
+
+        if (selectedOption === null) return "option";
+
+        if (index === challenge.correct_answer_id){
+            return "correct option"
+        }
+
+        if (selectedOption === index && index !== challenge.correct_answer_id){
+            return "option incorrect"
+        }
+
+        return "option"
+
+    }
+
+    return <div className="challenge-display">
+        <p><strong>Difficulty</strong>:{challenge.difficulty}</p>
+        <p className="challenge-title">{challenge.title}</p>
+
+
+        <div className="options">
+
+            {options.map((option,index) => (
+
+                <div 
+                className={getOptionClass(index)}
+                         key={index}
+                         onClick={() => handleOptionSelect(index)}
+
+
+                
+                >
+
+                    {option}
+
+
+                </div>
+
+            ))}
+
+
+        </div>
+
+            {shouldShowExplaination && selectedOption !== null && (
+                <div className="explanation" >
+
+                    <h4>Explaination</h4>
+                    <p>{challenge.explanation}</p>
+
+                </div>
+            )}
+
+    </div>
 
 } 
