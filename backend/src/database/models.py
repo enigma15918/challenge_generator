09e688f897f1a679d7713189e6f2306b73f5c798
enigma15918 <enigma15918@gmail.com>
2026-07-23 03:46:01 +0300

    end 90% from back end

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -14,5 +14,41 @@ class Challenge(Base):
 
     __tablename__="challenges"
 
-    
+    id =Column(Integer,primary_key=True)
+    difficulty=Column(String,nullable=False)
+    date_created=Column(DateTime,default=datetime.now)
+
+    created_by=Column(String,nullable=False)
+
+    title=Column(String,nullable=False)
+
+    options=Column(String,nullable=False)
+
+    correct_answer_id=Column(Integer,nullable=False)
+
+    explanation=Column(String,nullable=False)
+
+class ChallengeQuota(Base):
+    __tablename__="challenge_quotas"
+
+    id=Column(Integer,primary_key=True)
+    user_id=Column(String,nullable=False,unique=True)
+
+    remaining_quota=Column(Integer,nullable=False,default=50)
+
+    last_reset_date=Column(DateTime,default=datetime.now)
+
+Base.metadata.create_all(engine)
+
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+def get_db():
+    db=SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
+
 
